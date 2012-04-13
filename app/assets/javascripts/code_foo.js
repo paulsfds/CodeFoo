c50125ae190325096ffd8c7a53a438bb9fd24e76
Paul Wong <paulsfds@gmail.com>
2012-04-13 01:44:06 -0700

    got some of layout working

--- app/assets/javascripts/code_foo.js
+++ app/assets/javascripts/code_foo.js
@@ -1,12 +1,8 @@
 $(function () {
-    var $container = $('#container');
-
-    $container.imagesLoaded(function () {
-        $container.masonry({
-            itemSelector:'.box',
-            columnWidth: function(containerWidth) {
-                return containerWidth / 5;
-            }
-        });
+    Response.create({
+        prop: "width"  // "width" "device-width" "height" "device-height" or "device-pixel-ratio"
+      , prefix: "min-width- r src"  // the prefix(es) for your data attributes (aliases are optional)
+      , breakpoints: [1281,1025,961,641,481,320,0] // min breakpoints (defaults for width/device-width)
+      , lazy: true // optional param - data attr contents lazyload rather than whole page at once
     });
 });
